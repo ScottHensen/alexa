2322a9d0cd4ea6aca832a434f8245142192d640c
ScottHensen <hensen.scott@gmail.com>
2017-10-27 16:06:41 -0700

    add rough search to DeadShow

--- DeadShow/lambda/index.js
+++ DeadShow/lambda/index.js
@@ -3,14 +3,50 @@
 'use strict'
 
 const Alexa       = require('alexa-sdk')
+const https       = require('https')
 const APP_ID      = "amzn1.ask.skill.805dbbe6-3395-4772-b33e-d14823a264eb"
 const SKILL_NA    = "Dead Show"
-const MSG_PLAY    = "Now playing:  a not-so-random show"  // {{ audioData.title }}
 const MSG_PAUSE   = "Pausing"
 const MSG_RESUME  = "Resuming"
 const MSG_STOP    = "Later"
+const MSG_ABORT   = `Woah man, I just got a weird message from beyond.  Not sure what to do about that.`
+const MSG_NOT_FND = "I couldn't find anything for "
+const MSG_TRY_AGAIN = "Try again."
 const MSG_HELP    = "You can ask me to play a show."
+const MSG_LIST_HELP = "I didn't hear any search criteria.  I sure don't want to list every show.  Please ask me to list shows in which the band played a particular song, or shows from a particular year or venue."
 const PROMPT_HELP = "What do you want me to play?"
+const SEARCH_URL  = "https://archive.org/services/search/v1/scrape?fields=dir,title,date,subject,venue,source&sorts=date&q=collection%3A"
+const BAND_NA     = "GratefulDead"
+
+//TODO:  this object should be created with search result
+var audioData = {
+    title: 'Grateful Dead Live at Barton Hall on 1977-05-08',
+    subtitle: 'Dead Show',
+    cardContent: "Recorded by our beautiful brothers and sisters. \nHoused in the Internet Archives",
+    // url: 'https://archive.org/embed/gd1994-07-27.AKG451.Darby.119474.Flac1644&autoplay=1'
+    //url: 'https://archive.org/download/gd1994-07-27.AKG451.Darby.119474.Flac1644/gd1994-07-27.AKG451.t11.ogg&autoplay=1',
+    tracks: [
+        {
+            title: 'Tuning',
+            url: 'https://archive.org/download/gd1977-05-08.shure57.stevenson.29303.flac16/gd1977-05-08d01t01.ogg'
+        },
+        {
+            title: 'Take a Step Back',
+            url: 'https://archive.org/download/gd1977-05-08.shure57.stevenson.29303.flac16/gd1977-05-08d02t03.ogg'
+        },
+        {
+            title: 'St. Stephen',
+            url: 'https://archive.org/download/gd1977-05-08.shure57.stevenson.29303.flac16/gd1977-05-08d03t04.ogg'
+        }
+    ],
+    image: {  //TODO: fix these images to size properly
+        largeImageUrl: 'https://s3-us-west-2.amazonaws.com/deadco.show/img/grateful-sugar-skull.jpg',
+        smallImageUrl: 'https://s3-us-west-2.amazonaws.com/deadco.show/img/grateful-sugar-skull.jpg'
+      //smallImageUrl: 'https://s3-us-west-2.amazonaws.com/deadco.show/img/stealyourface_300x300.jpg'
+    }
+}
+var MSG_PLAY    = "Now playing: " + audioData.title
+var searchCriteria = ''
 
 var stateEventHandlers = {
     'LaunchRequest': function () {
@@ -20,16 +56,70 @@ var stateEventHandlers = {
         this.emit('PlayShow')
     },
     'PlayShow': function () {
-        //this.emit(':tell', MSG_PLAY)
-        //controller.play.call(this, this.t('WELCOME_MSG', { skillName: audioData.title } ));
         controller.play.call(this, MSG_PLAY);
     },
+    'ListIntent': function () {
+        let song  = this.event.request.intent.slots.song.value
+        let venue = this.event.request.intent.slots.venue.value
+        let year  = this.event.request.intent.slots.showYear.value
+        console.log('song:'+song + ', venue:'+venue + ',year:'+year)
+
+        // build search string
+        let searchUrl = SEARCH_URL + '(' + BAND_NA + ')'
+        // let searchResults = {}
+        if ((song === undefined) && (year === undefined) && (venue === undefined)) {
+            console.log('error:  no slots')
+            this.emit(':ask', MSG_LIST_HELP, PROMPT_HELP)
+        }
+        if (year !== undefined) {
+            searchUrl += ( '%20AND%20(' + year + ')' )
+            addToSearchCriteria(year)
+        }
+        if (venue !== undefined) {
+            searchUrl += ( '%20AND%20(' + venue + ')' )
+            addToSearchCriteria(venue)
+        }
+        if (song !== undefined) {
+            searchUrl += ( '%20AND%20(' + song + ')' )
+            addToSearchCriteria(song)
+        }
+        console.log(searchUrl)
+        var responseItems = {}
+        var responseString = ''
+
+        https.get(searchUrl, result => {
+            result.on('data', data => {
+                //console.log('data = ' + data )
+                responseString += data
+            })
+            result.on('end', () => {
+                //console.log('end. = ' + responseString)
+                responseItems = JSON.parse(responseString)
+                console.log('responseItems = ' + responseItems.toString())
+                if ( Object.keys(responseItems).length === 0 ||
+                    !Array.isArray(responseItems.items)      ||
+                    !responseItems.items.length                )
+                    {
+                        console.log('search came up empty')
+                        let msg = MSG_NOT_FND + song + MSG_TRY_AGAIN
+                        this.emit(':tell', msg)
+                }
+                else {
+                    console.log('first show = ' + responseItems.items[0].title)
+                    this.emit(':tell', responseItems.items[0].title)
+                }
+            })
+        }).on('error', err => {
+            console.log('error: ' + err.message)
+        })
+    },
     'AMAZON.PauseIntent': function () {
-        this.emit('AMAZON.StopIntent')    // TODO:  FIX
+        this.emit('AMAZON.StopIntent')
       //this.emit(':tell', MSG_PAUSE)
     },
     'AMAZON.ResumeIntent': function () {
-        this.emit(':tell', MSG_RESUME)    // TODO:  FIX?
+        controller.play.call(this, MSG_PLAY);  //TODO: fix to find song on pause
+      //this.emit(':tell', MSG_RESUME)
     },
     'AMAZON.CancelIntent': function () {
         this.emit('AMAZON.StopIntent')
@@ -37,11 +127,14 @@ var stateEventHandlers = {
     },
     'AMAZON.StopIntent': function () {
         controller.stop.call(this, MSG_STOP)
-      //this.emit(':tell', MSG_STOP)
     },
     'AMAZON.HelpIntent': function () {
         this.emit(':ask', MSG_HELP, PROMPT_HELP)
+    },
+    'Unhandled': function() {
+        this.emit(':tell', MSG_ABORT);
     }
+
 }
 
 var audioEventHandlers = {
@@ -53,6 +146,14 @@ var audioEventHandlers = {
         console.log('Playback Stopped')
         this.emit(':responseReady')
     },
+    'PlaybackNearlyFinished': function () {
+        console.log('Playback Nearly Finished: enqueue next song.')
+        this.response
+            .audioPlayerPlay('ENQUEUE', audioData.tracks[1].url, audioData.tracks[1].url, audioData.tracks[0].url, 0)
+        // this.response
+        //     .audioPlayerPlay('ENQUEUE', audioData.tracks[2].url, audioData.tracks[2].url, audioData.tracks[1].url, 0)
+        this.emit(':responseReady')
+    },
     'PlaybackFailed': function () {
         console.log('Playback Failed : %j', this.event.request.error)
         this.response.audioPlayerClearQueue('CLEAR_ENQUEUED')
@@ -60,56 +161,38 @@ var audioEventHandlers = {
     }
 }
 
-// TODO: url & image
-var audioData = {
-    title: 'Dead Show',
-    subtitle: 'Streaming Grateful Dead Concert Tapes',
-    cardContent: "Recorded by our beautiful brothers and sisters; housed in the Internet Archives",
-    // url: 'https://archive.org/details/gd1994-07-27.AKG451.Darby.119474.Flac1644&autoplay=1'
-    url: 'https://archive.org/download/gd1994-07-27.AKG451.Darby.119474.Flac1644/gd1994-07-27.AKG451.t11.ogg&autoplay=1',
-    image: {
-        largeImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-1200.png',
-        smallImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-720.png'
-    }
-}
-
 var controller = function () {
     return {
         play: function (text) {
-            /*
-             *  Using the function to begin playing audio when:
-             *      Play Audio intent invoked.
-             *      Resuming audio when stopped/paused.
-             *      Next/Previous commands issued.
-             */
-
-            if (canThrowCard.call(this)) {
-                var cardTitle   = audioData.subtitle;
+            if (canInsertCardIntoResponse.call(this)) {
+                var cardTitle   = audioData.title;
                 var cardContent = audioData.cardContent;
                 var cardImage   = audioData.image;
                 this.response.cardRenderer(cardTitle, cardContent, cardImage);
             }
 
-            this.response.speak(text).audioPlayerPlay('REPLACE_ALL', audioData.url, audioData.url, null, 0);
-            this.emit(':responseReady');
+            this.response
+                .speak(text)
+//              .audioPlayerPlay('REPLACE_ALL', audioData.url, audioData.url, null, 0)
+                .audioPlayerPlay('REPLACE_ALL', audioData.tracks[0].url, audioData.tracks[0].url, null, 0)
+                // .audioPlayerPlay('ENQUEUE', audioData.tracks[1].url, audioData.tracks[1].url, audioData.tracks[0].url, 0)
+                // .audioPlayerPlay('ENQUEUE', audioData.tracks[2].url, audioData.tracks[2].url, audioData.tracks[1].url, 0)
+
+            this.emit(':responseReady')
         },
         stop: function (text) {
-            /*
-             *  Issuing AudioPlayer.Stop directive to stop the audio.
-             *  Attributes already stored when AudioPlayer.Stopped request received.
-             */
             this.response.speak(text).audioPlayerStop();
             this.emit(':responseReady');
         }
     }
 }();
 
-function canThrowCard() {
-    /*
-     * To determine when can a card should be inserted in the response.
-     * In response to a PlaybackController Request (remote control events) we cannot issue a card,
-     * Thus adding restriction of request type being "IntentRequest".
-     */
+// var search = function (searchObj) {
+
+//     return searchObj
+// }();
+
+function canInsertCardIntoResponse() {
     if (this.event.request.type === 'IntentRequest' || this.event.request.type === 'LaunchRequest') {
         return true;
     } else {
@@ -117,6 +200,14 @@ function canThrowCard() {
     }
 }
 
+function addToSearchCriteria(criteria) {
+    if (searchCriteria === '') {
+        searchCriteria = criteria
+    }
+    else {
+        searchCriteria = searchCriteria + " and " + criteria
+    }
+}
 exports.handler = function (event, context) {
     const skill = Alexa.handler(event, context)
 
